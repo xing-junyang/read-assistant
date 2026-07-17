13c178f0457ce776af0562d934e85946cddb4af3
xingjunyang <xingjunyang@smail.nju.edu.cn>
2026-07-17 20:25:50 +0800

    Initial Commit

---
+++ read assistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  read assistant
+//
+//  Created by xingjunyang on 2026/7/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ read assistant/read_assistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  read_assistantApp.swift
+//  read assistant
+//
+//  Created by xingjunyang on 2026/7/17.
+//
+
+import SwiftUI
+
+@main
+struct read_assistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
